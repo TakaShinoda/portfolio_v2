56807aa8975239f0c3dff07c17147a10e9d87a1d
Takahiro <tkhr_621@docomo.ne.jp>
2019-12-19 18:34:55 +0900

    gatsby-imageによる写真表示機能の追加

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { Link } from 'gatsby';
+import { Link, StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
   const IndexPage = () => (
-  <Layout>
-    <div className="container-fluid p-0">
+  <StaticQuery
+    query={query}
+    render={ data => (
+    <Layout>
+      <div className="container-fluid p-0">
       <section
         className="resume-section p-3 p-lg-5 d-flex align-items-center"
         id="about"
@@ -13,11 +17,13 @@ import { Link } from 'gatsby';
               <h2 className="mb-5">Portfolio v1.0</h2>
               <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                   <div className="resume-content">
-                      <p>写真</p>
+                      <div>
+                          <Img fixed={data.file.childImageSharp.fixed} />
+                        </div>
                       <ul>
                           <li>URL: <a href="https://TakaShinoda.github.io/portfolio/" rel="noopener noreferrer" target="_blank">https://TakaShinoda.github.io/portfolio/</a></li>
                           <li>GitHub: <a href="https://github.com/TakaShinoda/portfolio" rel="noopener noreferrer" target="_blank">https://github.com/TakaShinoda/portfolio</a></li>      
-                          <li>HTML5, CSS3, JavaScript, GitHub</li>
+                          <li>HTML5, CSS3, JavaScript</li>
                           <li>This is the previous portfolio site.</li>
                         </ul>
                     </div>
@@ -29,6 +35,22 @@ import { Link } from 'gatsby';
       </section>
     </div>
   </Layout>
+  )}
+/>
 );
 
 export default IndexPage;
+
+
+
+const query = graphql`
+    query {
+        file(relativePath: {eq: "portfolio1.png"}) {
+            childImageSharp{
+                fixed(width: 450) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+    }
+`
