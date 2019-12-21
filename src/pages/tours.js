b5c92c2f10f6bcd59c7458115d17705fd34327fe
Takahiro <tkhr_621@docomo.ne.jp>
2019-12-22 01:08:19 +0900

    使用ツールの編集

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -23,7 +23,7 @@ import Img from 'gatsby-image';
                       <ul>
                           <li>URL: <a href="https://miyazaki-tours.firebaseapp.com/" rel="noopener noreferrer" target="_blank">https://miyazaki-tours.firebaseapp.com</a></li>
                           <li>GitHub: <a href="https://github.com/TakaShinoda/miyazaki-tours" rel="noopener noreferrer" target="_blank">https://github.com/TakaShinoda/miyazaki-tours</a></li>      
-                          <li>Vue.js, Firebase(Authentication, Realtime Database, Hosting)</li>
+                          <li>Vue.js, Firebase</li>
                           <li>It is a word-of-mouth site of a tourist spot.</li>
                         </ul>
                     </div>
